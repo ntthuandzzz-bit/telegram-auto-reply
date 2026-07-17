2180b00f0d059b1abda72506a0a21499eefe0d78
ntthuandzzz-bit <ntthuandzzz@gmail.com>
2026-07-18 00:42:51 +0700

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,16 @@
+class Logger:
+
+    @staticmethod
+    def info(text):
+
+        print(f"[INFO] {text}")
+
+    @staticmethod
+    def warning(text):
+
+        print(f"[WARNING] {text}")
+
+    @staticmethod
+    def error(text):
+
+        print(f"[ERROR] {text}")
