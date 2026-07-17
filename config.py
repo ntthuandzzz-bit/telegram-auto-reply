a326cebb1eaa17a3d8564e6c051fabfb6e873fe3
ntthuandzzz-bit <ntthuandzzz@gmail.com>
2026-07-18 00:39:13 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+
+API_HASH = os.getenv("API_HASH")
+
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+DATABASE_NAME = "database.db"
+
+DEFAULT_BLOCK = "welcome"
+
+REPLY_INTERVAL = 24 * 60 * 60
